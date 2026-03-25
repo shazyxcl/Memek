8f2a68602b2fe4aa22b677cd90835fccefccb39a
shazyxcl <161633828+shazyxcl@users.noreply.github.com>
2026-03-25 22:07:01 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinxSerialization)
+    kotlin("kapt") version libs.versions.kotlin.get()  // Tambahkan ini
 }
 
 android {
